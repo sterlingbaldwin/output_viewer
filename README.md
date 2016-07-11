9ea9adcb58cf5ed2f440f54f38eea264bb66f6fb
Sam Fries <fries2@llnl.gov>
2016-07-11 10:55:20 -0700

    1.0.0 commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Output Viewer
+
+The Output Viewer is designed to provide a framework for viewing arbitrary output from diagnostics scripts, metrics, or any program that produces a ton of different files that you want to look at it in an easy-to-use fashion. It also provides tooling that interacts with the data file (index.json) that gets generated, creating HTML pages based on the file with your output embedded in it, and also the ability to upload your results to a server to share with collaborators, sponsors, or anyone who's interested.
--- setup.py
+++ setup.py
@@ -4,17 +4,11 @@ import os
 
 setup(
     name="output_viewer",
-    version="0.0.3",
+    version="1.0.0",
     description="Framework for building web pages to examine output.",
     author="Sam Fries",
     author_email="fries2@llnl.gov",
-    license="BSD",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7"
-    ],
-    install_requires=[],
+    install_requires=["requests"],
     packages=find_packages(),
     include_package_data=True,
     scripts=["scripts/view_output", "scripts/view_output.py", "scripts/upload_output", "scripts/upload_output.py"]
