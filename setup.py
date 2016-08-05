7b9c3da67fc2ba7b3686769b03906c51848e7d43
Sam Fries <fries2@llnl.gov>
2016-08-05 13:00:56 -0700

    Updated version in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 
 setup(
     name="output_viewer",
-    version="1.0.0",
+    version="1.0.1",
     description="Framework for building web pages to examine output.",
     author="Sam Fries",
     author_email="fries2@llnl.gov",
